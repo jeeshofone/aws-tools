576a4350ddb198a036f5a570e5a11267b2d6472d
jeeshofone <will.laws@me.com>
2021-06-18 15:00:28 +1000

    removed ignorance of pagination

--- find-all-subnets.sh
+++ find-all-subnets.sh
@@ -9,7 +9,7 @@ IFS=$'\n' regionlist=$(aws ec2 describe-regions --query Regions[*].[RegionName] 
 for region in $regionlist
 do
      echo -e "\nListing all subnets in region:'$region'"
-     aws ec2 describe-subnets --region $region --query "Subnets[*]" --output json > regions/$region-subnets.json
+     aws ec2 describe-subnets --no-paginate --region $region --query "Subnets[*]" --output json > regions/$region-subnets.json
      jq -r '.[] | [.AvailabilityZone, .CidrBlock, .VpcId] | @csv' regions/$region-subnets.json  > regions/$region-subnets.csv
      cat regions/$region-subnets.csv >> all-subnets.csv
 
